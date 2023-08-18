1d5f95f1da801fc9e96c3fc8c510579254964aeb
linuxdaemon <linuxdaemon.irc@gmail.com>
2023-08-18 12:12:08 -0400

    Allow individual detections to override actions
    This allows a detection to define response actions such as
    notable, email, etc to override the global defaults.
    
    This also adds a config option to copy the default config in to the
    detection yaml on generation, to allow easier editing afterwards

--- contentctl/objects/config.py
+++ contentctl/objects/config.py
@@ -162,6 +162,4 @@ class Config(BaseModel, extra=Extra.forbid):
     build: ConfigBuild = ConfigBuild()
     enrichments: ConfigEnrichments = ConfigEnrichments()
     test: Union[TestConfig,None] = None 
-    
-
-
+    custom_deployment: bool = False
