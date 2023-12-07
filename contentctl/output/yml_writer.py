063985d0f12552dba7db3898451210116cdaba41
linuxdaemon <linuxdaemon.irc@gmail.com>
2023-12-07 13:00:50 -0500

    Format yaml strings on write

--- contentctl/output/yml_writer.py
+++ contentctl/output/yml_writer.py
@@ -1,8 +1,19 @@
+from pathlib import Path
+from typing import Union
 import yaml
 
+class MyDumper(yaml.Dumper):
+    def choose_scalar_style(self) -> str:
+        if self.analysis is None:
+            self.analysis = self.analyze_scalar(self.event.value)
+
+        if self.analysis.multiline or len(self.event.value) > self.best_width:
+            return '>'
+        
+        return super().choose_scalar_style()
 
 class YmlWriter:
     @staticmethod
-    def writeYmlFile(file_path: str, obj: dict) -> None:
-        with open(file_path, "w") as outfile:
-            yaml.dump(obj, outfile, default_flow_style=False, sort_keys=False)
+    def writeYmlFile(file_path: Union[str, Path], obj: dict) -> None:
+        with open(file_path, "w", encoding='utf-8') as outfile:
+            yaml.dump(obj, outfile, default_flow_style=False, sort_keys=False, Dumper=MyDumper, width=80)
